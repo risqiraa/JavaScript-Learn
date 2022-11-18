9b6ccf24caf04c7656c96cbde75c057cadea4fd6
risqiraa <risqiraacc@gmail.com>
2022-11-18 16:51:02 +0700

    01.getElementById().innerHTML

---
+++ index.js
@@ -0,0 +1,16 @@
+<h2>JavaScript dapat mengubah isi html</h2>
+    <img src="" alt="" id="demo" width="100%" />
+
+    <button
+      type="button"
+      onclick="document.getElementById('demo').src = '/src/img/img1.jpg' "
+    >
+      click
+    </button>
+
+    <button
+      type="button"
+      onclick="document.getElementById('demo').src = '/src/img/img2.png' "
+    >
+      click2
+    </button>

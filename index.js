6854802fe42398282bd122821c1f96c2c4007aa4
risqiraa <risqiraacc@gmail.com>
2022-11-18 17:07:29 +0700

    02.getElementById('').src=''

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-<h2>JavaScript dapat mengubah isi html</h2>
-    <img src="" alt="" id="demo" width="100%" />
-
-    <button
-      type="button"
-      onclick="document.getElementById('demo').src = '/src/img/img1.jpg' "
-    >
-      click
-    </button>
-
-    <button
-      type="button"
-      onclick="document.getElementById('demo').src = '/src/img/img2.png' "
-    >
-      click2
-    </button>
+console.log("halo");
